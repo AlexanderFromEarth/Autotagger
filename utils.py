754b039fb56f6ea51b0fa7590c12af675cb54239
AlexanderFromEarth <alexanderfromearth@gmail.com>
2020-07-16 17:01:06 +0500

    added more pretty output

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+from math import ceil
 from functools import reduce
+from itertools import islice
 
 
 def read_file(path, encoding='utf-8'):
@@ -8,3 +10,12 @@ def read_file(path, encoding='utf-8'):
 
 def compose(funcs):
     return lambda x: reduce(lambda prev, cur: cur(prev), funcs, x)
+
+
+def columnize(sequence, cols=2):
+    size = ceil(len(sequence) / cols)
+    slices = [
+        islice(sequence, pos, pos + size)
+        for pos in range(0, len(sequence), size)
+    ]
+    return zip(*slices)
